d3956b898c5a26e008d93f9034e65309c4b027ad
Tomohiro Tsutsumi <tsutsumi@team-chan3.com>
2026-03-24 23:12:11 +0900

    loadingのtypeを拡張する場合について

--- src/stores/globalLoadingStore.ts
+++ src/stores/globalLoadingStore.ts
@@ -1,6 +1,12 @@
 import { create } from "zustand";
 import { createLoadingStore, LoadingStore } from "./createLoadingStore";
 
+//LoadingStoreを拡張する場合はここ
+// type GlobalLoadingStore = LoadingStore & {
+//   //hoge: string;
+// };
+// その場合、以下のLoadingStoreをGlobalLoadingStoreに変更
+
 export const useGlobalLoadingStore = create<LoadingStore>((set, get, api) => ({
   // 共通ロジックを展開
   ...createLoadingStore()(set, get, api),
